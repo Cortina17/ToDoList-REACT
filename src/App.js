2710b3eb06fb19ef6ac1e26fc692539abe9b6823
Cortina17 <diego.cortinagarcia@gmail.com>
2022-01-04 18:14:07 +0100

    updating state of task

--- src/App.js
+++ src/App.js
@@ -6,25 +6,35 @@ import Todo from "./components/Todo";
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
-  
+
 
   function addTask(name) {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
-  
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed }
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
   const taskList = tasks.map(task => (
     <Todo
-        id={task.id}
-        name={task.name}
-        completed={task.completed}
-        key={task.id}
-      />
-    )
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+    />
+  )
   );
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
-  
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
