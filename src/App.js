7b1d81142de8fb5987b72d85bb6d56aedcca1850
Cortina17 <diego.cortinagarcia@gmail.com>
2022-01-06 17:41:44 +0100

    alerting task

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
 function App(props) {
+  function addTask(name) {
+    alert(name);
+  }  
   const taskList = props.tasks.map(task => (
     <Todo
         id={task.id}
@@ -16,7 +19,7 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
