fdb3a49b7923223f0b9da5ee650fbfde328f0fb0
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-04 22:58:25 +0100

    added component instead of repeating code

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App subject="Dear"/>
+    <App tasks={DATA}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
