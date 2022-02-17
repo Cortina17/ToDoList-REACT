efcde4385136c6dc0525215b741f6ef2c70a8c13
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-17 21:39:43 +0100

    first steps setting up the environment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App(props) {
+  const subject = props.subject;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, {subject}!
         </p>
         <a
           className="App-link"
