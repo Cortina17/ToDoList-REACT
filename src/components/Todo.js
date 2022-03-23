d2a9986b8b90a3cd2f567ff5dd0494bbeb5d6f77
Cortina17 <diego.cortinagarcia@gmail.com>
2022-03-23 19:33:48 +0100

    added focus to headline after delete a task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useRef, useState } from "react";
 
+
+function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+        ref.current = value;
+    });
+    return ref.current;
+}
+
+
 export default function Todo(props) {
     const [isEditing, setEditing] = useState(false);
     const [newName, setNewName] = useState('');
     const editFieldRef = useRef(null);
     const editButtonRef = useRef(null);
-
+    const wasEditing = usePrevious(isEditing);
+    
     function handleChange(e) {
         setNewName(e.target.value);
     }
@@ -71,10 +82,13 @@ export default function Todo(props) {
     );
 
     useEffect(() => {
-        if (isEditing) {
-            editFieldRef.current.focus();
+        if (!wasEditing && isEditing) {
+        editFieldRef.current.focus();
         }
-    }, [isEditing]);
+        if (wasEditing && !isEditing) {
+        editButtonRef.current.focus();
+        }
+    }, [wasEditing, isEditing]);
 
     return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
